c145507fe17b4900d7a12a6af444ba480e1c600f
andrei-papou <popow.andrej2009@yandex.ru>
2022-06-05 12:54:01 +0300

    Added subset size subclasses to root __init__.py

--- torch_iter/__init__.py
+++ torch_iter/__init__.py
@@ -1,7 +1,8 @@
 from .index import Index
-from .sized_iter import SizedIter
 from .metric import MetricCriteria
 from .planners.base import IterPlanner, IterPlannerBuilder
 from .planners.subset import FixedSubsetIterPlanner, FixedSubsetIterPlannerBuilder, \
     EpochBasedSubsetIterPlanner, EpochBasedSubsetIterPlannerBuilder, \
     MetricBasedSubsetIterPlanner, MetricBasedSubsetIterPlannerBuilder
+from .sized_iter import SizedIter
+from .subset_size import NatSubsetSize, FracSubsetSize
